a69bf866d8786a3d9297890cc2625fad1a0ccf92
Slighting <Slighting@SLIGHTING>
2023-12-28 17:22:33 +0800

    添加项目文件。

---
+++ WPFCG/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFCG.UserCtrl;
+
+namespace WPFCG
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Handle(object sender, RoutedEventArgs e)
+        {
+            var index = int.Parse((sender as Button).CommandParameter.ToString());
+            if (index == 1) this.WindowState = WindowState.Minimized;
+            if (index == 2) this.Close();
+            if (index == 3) XC.Content = new HardwareCheck();
+            if (index == 4) XC.Content = new HandwareInfo();
+            if (index == 5) { }
+            if (index == 6) { }
+            if (index == 7) { }
+            if (index == 8) { }
+        }
+    }
+}
---
+++ WPFCG/UserCtrl/HardwareCheck.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCG.UserCtrl
+{
+    /// <summary>
+    /// HardwareCheck.xaml 的交互逻辑
+    /// </summary>
+    public partial class HardwareCheck : UserControl
+    {
+        public HardwareCheck()
+        {
+            InitializeComponent();
+            ((Storyboard)FindResource("LoadingX")).Begin();
+            ((Storyboard)FindResource("LoadingY")).Begin();
+            ((Storyboard)FindResource("LoadingZ")).Begin();
+            this.CPUText.Text = "38";
+            this.GPUText.Text = "72";
+            this.RAMText.Text = "15";
+        }
+    }
+}
